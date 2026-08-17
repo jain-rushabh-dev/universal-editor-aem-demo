e9e1b018cdd0f63e9162e9f3b5ce66341ea14fe9
jain-rushabh-dev <rushabh.jain@axeno.co>
2026-08-17 23:40:05 +0530

    Updated Carousel Block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,13 +1,35 @@
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/css';
+import Splide from 'https://cdn.jsdelivr.net/npm/@splidejs/splide/+esm';
 
 export default function decorate(block) {
-  const splide = new Splide(block, {
-    type: 'loop',
-    perPage: 3,
-    gap: '1rem',
-    autoplay: true,
+
+  [...block.children].forEach((div) => {
+    div.className = 'splide__slide';
+
+    const [image, preTitle, preTitleType, title, description, button, alignContent] = [...div.children];
+
+    image.className = 'cmp-teaser__image';
+
   });
 
+  const slideTrack = document.createElement('div');
+  slideTrack.className = 'splide__track';
+  const slideList = document.createElement('div');
+  slideList.className = 'splide__list';
+  slideList.append(...block.children);
+  slideTrack.append(slideList);
+  block.replaceChildren(slideTrack);
+  block.className = 'splide';
+
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://cdn.jsdelivr.net/npm/@splidejs/splide/dist/css/splide.min.css';
+  link.dataset.splide = 'true';
+  document.head.appendChild(link);
+
+  const splide = new Splide('.splide', {
+    type: 'loop',
+    arrows: false,
+    padding: '5rem'
+  });
   splide.mount();
 }
