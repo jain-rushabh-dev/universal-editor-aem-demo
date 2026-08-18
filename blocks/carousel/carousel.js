4a6fc6246c55761f31d46c27672d058dc12468ad
jain-rushabh-dev <rushabh.jain@axeno.co>
2026-08-19 03:51:40 +0530

    Updated Carousel Block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,54 +1,86 @@
 import Splide from 'https://cdn.jsdelivr.net/npm/@splidejs/splide/+esm';
 
+function createOptimizedPicture(image, mobileImage) {
+  const picture = document.createElement('picture');
+
+  const source = document.createElement('source');
+  source.media = '(max-width: 1199px)';
+  source.srcset = mobileImage?.querySelector('img')?.src?.replace(/\?.*/,'');
+
+  const img = document.createElement('img');
+  img.src = image?.querySelector('img')?.src?.replace(/\?.*/,'');
+  img.alt = image?.querySelector('img')?.alt || '';
+
+  picture.append(source, img);
+  image.className = 'cmp-teaser__image';
+  image.replaceChildren(picture);
+}
+
+function isValidElement(ele) {
+  if (ele) {
+    return ele;
+  }
+  return "";
+}
+
+function decorateAndMountSplide(block) {
+  const slideList = document.createElement('div');
+  slideList.className = 'splide__list';
+  slideList.append(...block.children);
+
+  const slideTrack = document.createElement('div');
+  slideTrack.className = 'splide__track';
+  slideTrack.append(slideList);
+
+  block.className = 'splide';
+  block.replaceChildren(slideTrack);
+
+  const splide = new Splide('.splide', {
+    type: 'loop',
+    arrows: false,
+    padding: '6.52174%'
+  });
+  setTimeout(() => {
+
+    splide.mount();
+  }, 50);
+}
+
 export default function decorate(block) {
-
   [...block.children].forEach((div) => {
-    div.className = 'splide__slide';
-
     let [image, mobileImage, preTitle, title, description, button, alignContent] = [...div.children];
 
-    image.className = 'cmp-teaser__image';
+    // Decorate Carousel Image for Desktop and Mobile View
+    createOptimizedPicture(image, mobileImage);
+
+    // Decorate PreTitle
+    preTitle = preTitle.firstElementChild;
+    if(preTitle) {
+      preTitle.className = 'cmp-teaser__pretitle';
+    }
+
+    // Decorate Title
+    title = title.firstElementChild;
+    if(title) {
+      title.className = 'cmp-teaser__title';
+    }
+
+    // Decorate Description
     description.className = 'cmp-teaser__description';
 
-    const picture = document.createElement('picture');
-
-    const source = document.createElement('source');
-    source.media = '(max-width: 767px)';
-    source.srcset = mobileImage.querySelector('img').src;
-
-    const img = document.createElement('img');
-    img.src = image.querySelector('img').src;
-    img.alt = image.querySelector('img').alt || '';
-
-    picture.append(source, img);
-    mobileImage.remove();
-    image.replaceChildren(picture);
-
-    if (preTitle.firstElementChild) {
-      preTitle.firstElementChild.className = 'cmp-teaser__pretitle';
-      preTitle = preTitle.firstElementChild;
-    } else {
-//      preTitle.remove();
-    }
-    if (title.firstElementChild) {
-       title.firstElementChild.className = 'cmp-teaser__title';
-       title = title.firstElementChild;
-    } else {
-//       preTitle.remove();
-    }
+    // Decorate Button
     const buttonLink = button.querySelector('a');
     if (buttonLink) {
       button.className = 'cmp-teaser__action-container';
       buttonLink.className = 'cmp-teaser__action-link';
       button.replaceChildren(buttonLink);
-    } else {
-      button.remove();
     }
 
+    // Decorate Teaser Content Block
     const teaserContentDiv = document.createElement('div');
     teaserContentDiv.className = 'cmp-teaser__content';
-    teaserContentDiv.append(preTitle);
-    teaserContentDiv.append(title);
+    teaserContentDiv.append(isValidElement(preTitle));
+    teaserContentDiv.append(isValidElement(title));
     teaserContentDiv.append(description);
     teaserContentDiv.append(button);
 
@@ -56,38 +88,11 @@ export default function decorate(block) {
     teaserDiv.className = 'teaser';
     teaserDiv.append(teaserContentDiv);
     teaserDiv.append(image);
+    alignContent.textContent == 'left' ? teaserDiv.classList.add('content-on-bottom', 'content-on-left') :
+      teaserDiv.classList.add('content-on-top', 'content-on-right');
 
-    if (alignContent.textContent == 'left') {
-      teaserDiv.classList.add('content-on-bottom');
-      teaserDiv.classList.add('content-on-left');
-    } else {
-      teaserDiv.classList.add('content-on-top');
-      teaserDiv.classList.add('content-on-right');
-    }
-    alignContent.remove();
-
-    div.append(teaserDiv);
+    div.className = 'splide__slide';
+    div.replaceChildren(teaserDiv);
   });
-
-  const slideTrack = document.createElement('div');
-  slideTrack.className = 'splide__track';
-  const slideList = document.createElement('div');
-  slideList.className = 'splide__list';
-  slideList.append(...block.children);
-  slideTrack.append(slideList);
-  block.replaceChildren(slideTrack);
-  block.className = 'splide';
-
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'https://cdn.jsdelivr.net/npm/@splidejs/splide/dist/css/splide.min.css';
-  link.dataset.splide = 'true';
-  document.head.appendChild(link);
-
-  const splide = new Splide('.splide', {
-    type: 'loop',
-    arrows: false,
-    padding: '6.52174%'
-  });
-  splide.mount();
+  decorateAndMountSplide(block);
 }
